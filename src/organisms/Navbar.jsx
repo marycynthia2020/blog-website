710289cec7f0a40e8dce2f1bbb2e21ae9bfcf635
Chinemerem Ugbaja <marycynthiaconfirm@gmail.com>
2025-10-07 20:31:48 +0000

    Update Navbar

--- src/organisms/Navbar.jsx
+++ src/organisms/Navbar.jsx
@@ -17,7 +17,6 @@ const Navbar = () => {
     <section className="z-[999] fixed w-full shadow-lg py-6 bg-white  mb-6 md:mb-10 text-[#777777] text-lg ">
         <nav className=" px-4 font-semibold max-w-[1440px] mx-auto text-[#777777] text-lg flex items-center justify-between">
             <div className='flex items-center gap-4'>
-              <Hamburger isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
               <Logo />
             </div>
             <div className=" hidden md:flex md:gap-8 md:items-center md:justify-between">
@@ -28,15 +27,17 @@ const Navbar = () => {
                 {!isLoggedIn && <SignupBtn />}
                 {isLoggedIn ? <LogoutBtn /> : <LoginBtn />}
             </div>
-            <div className='flex items-center gap-4 md:hidden'>
-              <ExploreBtn />
-            </div>
+            {/* <div className='flex items-center gap-4 md:hidden'>
+             
+            </div> */}
+            <Hamburger isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
         </nav>
         {isMenuOpen && <nav className='absolute flex flex-col h-screen gap-y-4 bg-white w-1/2 px-6 mt-6 py-6' onClick={()=>setIsMenuOpen(false)}>
             <HomeBtn />
             {isLoggedIn && <CreatePostBtn />}
             {isLoggedIn && <MyBlog />}
             {isLoggedIn ? <LogoutBtn /> : <LoginBtn />}
+           <ExploreBtn />
           </nav>}
 
     </section>
