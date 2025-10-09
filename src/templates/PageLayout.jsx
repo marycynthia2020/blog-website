03179e832a12fe5ad692df3ac234694e549f28d2
Chinemerem Ugbaja <marycynthiaconfirm@gmail.com>
2025-10-09 07:22:35 +0000

    Update PageLayout.jsx

--- src/templates/PageLayout.jsx
+++ src/templates/PageLayout.jsx
@@ -2,14 +2,17 @@ import React from 'react'
 import Navbar from '../organisms/Navbar'
 import { Outlet } from 'react-router'
 import Footer from '../organisms/Footer'
+import { useLocation } from "react-router-dom";
 
 const PageLayout = () => {
+  const location = useLocation();
+  const hideFooter = location.pathname === "/signup" || "/login";
   return (
     <div  className='min-h-screen grid grid-rows-[auto_1fr_auto]'>
         <Navbar />
         <div className='mt-28'></div>
         <Outlet  />
-        <Footer />
+        {!hideFooter && <Footer />}
     </div>
   )
 }
